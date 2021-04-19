52f1ddbe487811a01f0f73d4f983b6dc42f9bb92
andrew <andrey.nik.999@gmail.com>
2021-04-19 17:49:31 +0700

    Change web proj struct

---
+++ Production_planing_web/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Production_planing_web.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: ApiController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Model()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
